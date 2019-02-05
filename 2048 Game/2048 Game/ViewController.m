ef9386499721479f8d51a7317dc737023269e1d9
Cameron Viezel <cviezel1@bl-n04.pods.bu.int>
2019-02-05 11:08:09 -0500

    implemented all game functionality on iOS and added colors to the numbers

--- 2048 Game/2048 Game/ViewController.m
+++ 2048 Game/2048 Game/ViewController.m
@@ -9,8 +9,28 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UILabel *label1;
-@property (weak, nonatomic) IBOutlet UILabel *label2;
+@property (weak, nonatomic) IBOutlet UILabel *l1;
+@property (weak, nonatomic) IBOutlet UILabel *l2;
+@property (weak, nonatomic) IBOutlet UILabel *l3;
+@property (weak, nonatomic) IBOutlet UILabel *l4;
+@property (weak, nonatomic) IBOutlet UILabel *l5;
+@property (weak, nonatomic) IBOutlet UILabel *l6;
+@property (weak, nonatomic) IBOutlet UILabel *l7;
+@property (weak, nonatomic) IBOutlet UILabel *l8;
+@property (weak, nonatomic) IBOutlet UILabel *l9;
+@property (weak, nonatomic) IBOutlet UILabel *l10;
+@property (weak, nonatomic) IBOutlet UILabel *l11;
+@property (weak, nonatomic) IBOutlet UILabel *l12;
+@property (weak, nonatomic) IBOutlet UILabel *l13;
+@property (weak, nonatomic) IBOutlet UILabel *l14;
+@property (weak, nonatomic) IBOutlet UILabel *l15;
+@property (weak, nonatomic) IBOutlet UILabel *l16;
+@property (weak, nonatomic) IBOutlet UIButton *up;
+@property (weak, nonatomic) IBOutlet UIButton *right;
+@property (weak, nonatomic) IBOutlet UIButton *down;
+@property (weak, nonatomic) IBOutlet UIButton *left;
+@property (weak, nonatomic) IBOutlet UILabel *lwinlose;
+
 
 @end
 
@@ -18,17 +38,70 @@
 
 int board[4][4];
 bool winFlag = false;
-UILabel * screen[2];
+bool gameFlag = true;
+UILabel * screen[4][4];
 
 NSMutableString *s;
 
 - (void)clearBoard{
+    _lwinlose.text = @"";
     for(int i = 0; i < 4; i++)
     {
         for(int j = 0; j < 4; j++)
         {
             board[i][j] = 0;
         }
+    }
+}
+- (void)colorLabel:(UILabel *)x
+{
+    if([x.text  isEqual: @"0"]) //0
+    {
+        [x setTextColor: [UIColor blackColor]];
+    }
+    if([x.text  isEqual: @"2"]) //2
+    {
+        [x setTextColor: [UIColor grayColor]];
+    }
+    if([x.text  isEqual: @"4"]) //4
+    {
+        [x setTextColor: [UIColor lightGrayColor]];
+    }
+    if([x.text  isEqual: @"8"]) //8
+    {
+        [x setTextColor: [UIColor brownColor]];
+    }
+    if([x.text  isEqual: @"16"]) //16
+    {
+        [x setTextColor: [UIColor purpleColor]];
+    }
+    if([x.text  isEqual: @"32"]) //32
+    {
+        [x setTextColor: [UIColor blueColor]];
+    }
+    if([x.text  isEqual: @"64"]) //64
+    {
+        [x setTextColor: [UIColor orangeColor]];
+    }
+    if([x.text  isEqual: @"128"]) //128
+    {
+        [x setTextColor: [UIColor redColor]];
+    }
+    if([x.text  isEqual: @"256"]) //256
+    {
+        [x setTextColor: [UIColor magentaColor]];
+    }
+    if([x.text  isEqual: @"512"]) //512
+    {
+        [x setTextColor: [UIColor greenColor]];
+    }
+    if([x.text  isEqual: @"1024"]) //1024
+    {
+        [x setTextColor: [UIColor yellowColor]];
+    }
+    if([x.text  isEqual: @"2048"]) //2048
+    {
+        [x setTextColor: [UIColor cyanColor]];
     }
 }
 - (void)printBoard
@@ -40,7 +113,9 @@ NSMutableString *s;
             //NSMutableString * temp = [NSMutableString stringWithString:@"s"];
             NSMutableString * temp = [NSMutableString stringWithFormat:@"%d\t", board[i][j]];
             [s appendString:temp];
-            NSLog(@"%d", board[i][j]);
+            NSLog(@"%d (%d,%d)", board[i][j], i, j);
+            screen[i][j].text = [NSString stringWithFormat:@"%d", board[i][j]];
+            [self colorLabel:screen[i][j]];
         }
         NSLog(@"\n");
         [s appendString:@"\n"];
@@ -76,7 +151,9 @@ NSMutableString *s;
     else
     {
         winFlag = false;
+        gameFlag = false;
         //cout << "game over" << endl;
+        _lwinlose.text = @"You Lose!";
         //game over
     }
 }
@@ -86,27 +163,212 @@ NSMutableString *s;
     {
         //cout << "You win!" << endl;
         winFlag = true;
+        gameFlag = false;
+        _lwinlose.text = @"You Win!";
     }
 }
+-(void)swipeUp
+{
+    for(int j = 0; j < 4; j++)
+    {
+        NSLog(@"hello");
+        for(int i = 1; i < 4; i++)
+        {
+            int n = 0;
+            while(n < 3)
+            {
+                for(int k = 3; k > 0; k--)
+                {
+                    if(board[k-1][j] == 0)
+                    {
+                        board[k-1][j] = board[k][j];
+                        board[k][j] = 0;
+                    }
+                }
+                n++;
+            }
+            if(board[i-1][j] == board[i][j] && board[i][j] != 0)
+            {
+                board[i-1][j] = 2*board[i][j];
+                [self checkWin: board[i-1][j]];
+                board[i][j] = 0;
+                for(int k = 3; k > 0; k--)
+                {
+                    if(board[k-1][j] == 0)
+                    {
+                        board[k-1][j] = board[k][j];
+                        board[k][j] = 0;
+                    }
+                }
+            }
+        }
+    }
+    [self generateNewTile];
+}
+-(void) swipeDown
+{
+    for(int j = 0; j < 4; j++)
+    {
+        for(int i = 2; i >= 0; i--)
+        {
+            int n = 0;
+            while(n < 3)
+            {
+                for(int k = 0; k < 3; k++)
+                {
+                    if(board[k+1][j] == 0)
+                    {
+                        board[k+1][j] = board[k][j];
+                        board[k][j] = 0;
+                    }
+                }
+                n++;
+            }
+            if(board[i+1][j] == board[i][j] && board[i][j] != 0)
+            {
+                board[i+1][j] = 2*board[i][j];
+                [self checkWin:board[i+1][j]];
+                board[i][j] = 0;
+                for(int k = 0; k < 3; k++)
+                {
+                    if(board[k+1][j] == 0)
+                    {
+                        board[k+1][j] = board[k][j];
+                        board[k][j] = 0;
+                    }
+                }
+            }
+        }
+    }
+    [self generateNewTile];
+}
+- (void) swipeLeft
+{
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 1; j < 4; j++)
+        {
+            int n = 0;
+            while(n < 3)
+            {
+                for(int k = 3; k > 0; k--)
+                {
+                    if(board[i][k-1] == 0)
+                    {
+                        board[i][k-1] = board[i][k];
+                        board[i][k] = 0;
+                    }
+                }
+                n++;
+            }
+            if(board[i][j-1] == board[i][j] && board[i][j] != 0)
+            {
+                board[i][j-1] = 2*board[i][j];
+                [self checkWin: board[i][j-1]];
+                board[i][j] = 0;
+                for(int k = 3; k > 0; k--)
+                {
+                    if(board[i][k-1] == 0)
+                    {
+                        board[i][k-1] = board[i][k];
+                        board[i][k] = 0;
+                    }
+                }
+            }
+        }
+    }
+    [self generateNewTile];
+}
+- (void) swipeRight
+{
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 2; j >= 0; j--)
+        {
+            int n = 0;
+            while(n < 3)
+            {
+                for(int k = 0; k < 3; k++)
+                {
+                    if(board[i][k+1] == 0)
+                    {
+                        board[i][k+1] = board[i][k];
+                        board[i][k] = 0;
+                    }
+                }
+                n++;
+            }
+            if(board[i][j+1] == board[i][j] && board[i][j] != 0)
+            {
+                board[i][j+1] = 2*board[i][j];
+                [self checkWin: board[i][j+1]];
+                board[i][j] = 0;
+                for(int k = 0; k < 3; k++)
+                {
+                    if(board[i][k+1] == 0)
+                    {
+                        board[i][k+1] = board[i][k];
+                        board[i][k] = 0;
+                    }
+                }
+            }
+        }
+    }
+    [self generateNewTile];
+}
+- (IBAction)pressUp:(id)sender {
+    if(gameFlag)
+        [self swipeUp];
+    [self printBoard];
+}
+- (IBAction)pressRight:(id)sender {
+    if(gameFlag)
+        [self swipeRight];
+    [self printBoard];
+}
+- (IBAction)pressDown:(id)sender {
+    if(gameFlag)
+        [self swipeDown];
+    [self printBoard];
+}
+- (IBAction)pressLeft:(id)sender {
+    if(gameFlag)
+        [self swipeLeft];
+    [self printBoard];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    screen[0][0] = _l1;
+    screen[0][1] = _l2;
+    screen[0][2] = _l3;
+    screen[0][3] = _l4;
+    screen[1][0] = _l5;
+    screen[1][1] = _l6;
+    screen[1][2] = _l7;
+    screen[1][3] = _l8;
+    screen[2][0] = _l9;
+    screen[2][1] = _l10;
+    screen[2][2] = _l11;
+    screen[2][3] = _l12;
+    screen[3][0] = _l13;
+    screen[3][1] = _l14;
+    screen[3][2] = _l15;
+    screen[3][3] = _l16;
+    
+    
     [self clearBoard];
-    [self generateNewTile];
+    [self printBoard];
     [self generateNewTile];
     [self generateNewTile];
     [self printBoard];
+    //[self swipeUp];
+    //[self printBoard];
     
-    NSLog(@"test!");
+    NSLog(@"test");
     
-    screen[0] = _label1;
-    screen[1] = _label2;
-
-    screen[0].text = @"2";
-    screen[1].text = @"4";
-    
-    NSLog(@"%@", s);
 }
 
 
